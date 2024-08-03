b264e9e30fc00b70b73ce69016ca49c118bbcaca
Aaron Graves <ajgraves@ajgraves.com>
2024-08-03 16:58:01 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Aaron Graves on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
