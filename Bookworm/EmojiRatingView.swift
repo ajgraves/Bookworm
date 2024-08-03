dc43bee7751ef834e4f467c1150e44fc79b0e985
Aaron Graves <ajgraves@ajgraves.com>
2024-08-03 18:24:39 -0500

    Completed "Building a list with @Query" module

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,35 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Aaron Graves on 8/3/24.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("😒")
+            
+        case 2:
+            Text("🥱")
+            
+        case 3:
+            Text("🫤")
+            
+        case 4:
+            Text("😊")
+            
+        default:
+            Text("🤯")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
